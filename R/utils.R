26a6e11cd9dbab919a4ff858e840a380ea8c6695
JBGruber <JohannesB.Gruber@gmail.com>
2024-06-19 16:58:57 +1000

    rewrite install

--- R/utils.R
+++ R/utils.R
@@ -1,24 +1,5 @@
-#' init_instar
-#'
-#' @description Initializes the instaloader python functions. Only works when \code{install_instar()} has ben executed.
-#' @export
-init_instar <- function() {
-  reticulate::source_python(system.file("python", "instaloader.py",
-    package = "instar"
-  ))
-
-  message("instaloader initialized")
-}
-
-#' install_instar
-#'
-#'
-#' @description Installs the instaloader Python module
-#' @param envname specify Python environment name for module installation
-#' @export
-install_instar <- function(envname = NULL) {
-  reticulate::py_install(c("instaloader"), pip = TRUE, envname = envname)
-}
+the <- new.env()
+the$fail_counter <- 0
 
 #' from_unix
 #'
